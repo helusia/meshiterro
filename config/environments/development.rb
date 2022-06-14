efed6a5d88088046c313fd7e6c4d7315f83b94a3
helusia <helusiasparkling@gmail.com>
2022-06-14 14:46:32 +0000

    サーバー立ち上げ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "d1d8036f8e184848a6dcab01b0b3915f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
