26cc4088bca0b2805fb5b4d73f4ce54608231710
helusia <helusiasparkling@gmail.com>
2022-06-27 13:45:32 +0000

    16syou

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(user_params)
+    @user.update
+    redirect_to user_path(@user.id)
   end
 
   def show
     @user =User.find(params[:id])
-    @post_images = @user.post_images 
+    @post_images = @user.post_images
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
 
